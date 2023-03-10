f19b20a2b2c9fc61b4591bb1bae2148407dfe864
rmmenezes <ra29fa@gmail.com>
2023-03-10 13:58:48 -0300

    .

--- APP/appcertificate/lib/views/cert_generate.dart
+++ APP/appcertificate/lib/views/cert_generate.dart
@@ -8,6 +8,8 @@ import 'package:appcertificate/views/widgets/buttons.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 // ignore: camel_case_types
 class certGenerate extends StatefulWidget {
@@ -31,6 +33,72 @@ class _certGenerateState extends State<certGenerate> {
   void initState() {
     super.initState();
     _imageFile = null;
+
+    editpdf();
+  }
+
+  void findTextOnPdfDocument(PdfDocument pdfDoc, String searchText) {
+    for (int i = 0; i < pdfDoc.pages.count; i++) {
+      final PdfTextExtractor extractor = PdfTextExtractor(pdfDoc);
+      final String pageText = extractor.extractText();
+      if (pageText.contains(searchText)) {
+        final List<TextLine> textLines = extractor.extractTextLines();
+        for (TextLine line in textLines) {
+          if (line.text.contains(searchText)) {
+            final double x = line.bounds.left;
+            final double y = line.bounds.top;
+            print('Found "$searchText" at ($x, $y) on page ${i + 1}');
+          }
+        }
+      }
+    }
+  }
+
+  Future<void> editpdf() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/input.pdf';
+
+    final PdfDocument document =
+        PdfDocument(inputBytes: File(path).readAsBytesSync());
+
+    PdfPage page = document.pages[0];
+
+    page.graphics.drawImage(
+        PdfBitmap(File('assets/avataJoia.png').readAsBytesSync()),
+        const Rect.fromLTWH(20.512, 127.00, 75.00, 73.836));
+
+    page.graphics.drawString(
+        '238-879-621-524', PdfStandardFont(PdfFontFamily.helvetica, 10),
+        bounds: const Rect.fromLTWH(118.797, 131.454, 157.105, 15));
+
+    page.graphics.drawString(
+        'Prata', PdfStandardFont(PdfFontFamily.helvetica, 10),
+        bounds: const Rect.fromLTWH(143.698, 157.988, 131.062, 15));
+
+    page.graphics.drawString(
+        '22/02/2023', PdfStandardFont(PdfFontFamily.helvetica, 10),
+        bounds: const Rect.fromLTWH(156.049, 184.072, 119.437, 15));
+
+    page.graphics.drawString(
+        'Rafael Menezes Barboza', PdfStandardFont(PdfFontFamily.helvetica, 10),
+        bounds: const Rect.fromLTWH(62.794, 208, 214.647, 20));
+
+    page.graphics.drawString(
+        '452.117.598-85', PdfStandardFont(PdfFontFamily.helvetica, 10),
+        bounds: const Rect.fromLTWH(46.709, 231.390, 229.028, 15));
+
+    page.graphics.drawString(
+        'O conjunto de joias faz uso de banho tecnologico com pedras e banho de ouro na codificação exeistente entre o amor e a vida na direão correspondente a cada um estado da arte.',
+        PdfStandardFont(
+          PdfFontFamily.helvetica,
+          10,
+        ),
+        bounds: const Rect.fromLTWH(23.340, 253.778, 251.981, 60.560),
+        format: PdfStringFormat(alignment: PdfTextAlignment.justify));
+
+    final outputPath = '${directory.path}/output.pdf';
+    File(outputPath).writeAsBytes(await document.save());
+    document.dispose();
   }
 
   @override
